1e24a2a2c175a29fac6cd6f026ed9409cdcd4cda
Zoltán Sebestyén <44263870+BronsonTheGoat@users.noreply.github.com>
2025-03-28 17:55:27 +0100

    hw_01 book modell created and registerd to admin

--- library_app/library_application/admin.py
+++ library_app/library_application/admin.py
@@ -1,3 +1,16 @@
 from django.contrib import admin
 
+from .models import Book, Author
+
 # Register your models here.
+
+class BookAdmin(admin.ModelAdmin):
+    list_display = ['id', 'isbn', 'title', 'author', 'published_year', 'pages']
+    
+
+admin.site.register(Book, BookAdmin)
+
+# class AuthorAdmin(admin.ModelAdmin):
+#     list_display = ['id', 'name', 'birth_date']
+
+# admin.site.register(Author, AuthorAdmin)
