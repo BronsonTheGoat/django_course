05cdc45f52cfa7990ccd86f5d7e705b3c9d3a56a
Zoltán Sebestyén <44263870+BronsonTheGoat@users.noreply.github.com>
2025-04-04 19:48:37 +0200

    connect models, correcting connection type

--- library_app/library_application/models.py
+++ library_app/library_application/models.py
@@ -1,18 +1,8 @@
 from django.db import models
 
 # Create your models here.
-
-class Book(models.Model):
-    isbn = models.CharField(max_length=13, unique=True)
-    title = models.CharField(max_length=200)
-    author = models.CharField(max_length=200)
-    published_year = models.IntegerField()
-    pages = models.IntegerField()
     
-    def __str__(self):
-        return f"{self.title} - {self.author}"
-    
-class Autor(models.Model):
+class Author(models.Model):
     name = models.CharField(max_length=100)
     birth_date = models.DateField()
     birth_place = models.CharField(max_length=50)
@@ -22,3 +12,13 @@ class Autor(models.Model):
     
     def __str__(self):
         return f"{self.name}"
+
+class Book(models.Model):
+    isbn = models.CharField(max_length=13, unique=True)
+    title = models.CharField(max_length=200)
+    author = models.ForeignKey(Author, on_delete=models.CASCADE, related_name="author")
+    published_year = models.IntegerField()
+    pages = models.IntegerField()
+    
+    def __str__(self):
+        return f"{self.title} - {self.author}"
