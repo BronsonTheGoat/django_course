eb1f4f3db166b5b0f9c42662f27fb80530e042d0
Zoltán Sebestyén <44263870+BronsonTheGoat@users.noreply.github.com>
2025-04-04 20:01:00 +0200

    register author model and set dislplay, filter, ordering...

--- library_app/library_application/admin.py
+++ library_app/library_application/admin.py
@@ -1,18 +1,25 @@
 from django.contrib import admin
 
-from .models import Book
+from .models import Book, Author
 
 # Register your models here.
 
+class BookInline(admin.StackedInline):
+    model = Book
+    extra = 0
+
 class BookAdmin(admin.ModelAdmin):
-    list_display = ['title', 'author', 'published_year']
-    list_filter = ['published_year', 'author']
-    ordering = ['title', 'published_year']
+    list_display = ["title", "author", "published_year"]
+    list_filter = ["published_year", "author"]
+    ordering = ["title", "published_year"]
     
 
 admin.site.register(Book, BookAdmin)
 
-# class AuthorAdmin(admin.ModelAdmin):
-#     list_display = ['id', 'name', 'birth_date']
+class AuthorAdmin(admin.ModelAdmin):
+    list_display = ["id", "name", "birth_date", "birth_place"]
+    list_filter = ["nationality"]
+    ordering = ["name"]
+    inlines = [BookInline]
 
-# admin.site.register(Author, AuthorAdmin)
+admin.site.register(Author, AuthorAdmin)
